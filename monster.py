cdb081ce700a1205504998fc5c5228aacb671077
Amy Rose Lazarte <lazartea@reed.edu>
2016-11-30 21:20:20 -0800

    Update monster.py

--- monster.py
+++ monster.py
@@ -2,15 +2,25 @@ import random
 import updater
 
 class Monster:
-    def __init__(self, name, health, room):
+    def __init__(self, name, health, room, prob, damage):
         self.name = name
         self.health = health
         self.room = room
+        self.prob = prob
+        self.damage = damage
         room.addMonster(self)
         updater.register(self)
     def update(self):
         if random.random() < .5:
             self.moveTo(self.room.randomNeighbor())
+
+        if self.name == "Pterodactyl":
+            if random.random() < .2:
+                player.health -= 10
+                self.health += 10
+                player.money -= 10
+                print("A Pterodactyl swoops in and attacks. You lose 10 health and 10 money.")
+
     def moveTo(self, room):
         self.room.removeMonster(self)
         self.room = room
@@ -18,29 +28,3 @@ class Monster:
     def die(self):
         self.room.removeMonster(self)
         updater.deregister(self)
-
-class Velociraptor(Monster):
-    name = 'Velociraptor'
-    health = '75hp'
-    def __init__(self):
-        Monster.__init__(self, name, health, room)
-    
-class Argentinosaurus(Monster):
-    name = 'Argentinosaurus'
-    health = '200hp'
-    
-    def __init__(self):
-        Monster.__init__(self, name, health, room)
-    
-class Stegosaurus(Monster):
-    health = '150hp'
-    name = 'Stegosaurus'
-    def __init__(self):
-        Monster.__init__(self, name, health, room)
-        
-class TRex(Monster):
-    health = '150hp'
-    name = 'Angry T-Rex'
-    def __init__(self):
-        Monster.__init__(self, name, health, room)
-        
