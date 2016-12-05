131b61c2eb2e8cf0e19ef6e2dc8997631cebec10
Amy Rose Lazarte <lazartea@reed.edu>
2016-12-04 17:56:36 -0800

    Update monster.py
    added subclasses

--- monster.py
+++ monster.py
@@ -2,32 +2,41 @@ import random
 import updater
 
 class Monster:
-    def __init__(self, name, health, room, prob, damage):
+    def __init__(self, name, health, prob, damage):
         self.name = name
         self.health = health
-        self.room = room
+        self.loc = None
         self.prob = prob
         self.damage = damage
-        room.addMonster(self)
+        
         updater.register(self)
     def update(self):
         if random.random() < .5:
-            if self.name != "Angry T-Rex":
-                self.moveTo(self.room.randomNeighbor())
+            self.moveTo(self.loc.randomNeighbor())
 
-#         if self.name == "Pterodactyl":
-#             if random.random() < .2:
-#                 player.health -= 10
-#                 self.health += 10
-#                 player.money -= 10
-#                 print("A Pterodactyl swoops in and attacks. You lose 10 health and 10 money.")
+        if self.name == "Pterodactyl":
+            if random.random() < .2:
+                player.health -= 10
+                self.health += 10
+                player.money -= 10
+                print("A Pterodactyl swoops in and attacks. You lose 10 health and 10 money.")
 
-
-    def moveTo(self, room):
-        self.room.removeMonster(self)
-        self.room = room
+    def putInRoom(self, room):
+        self.loc = room
         room.addMonster(self)
+
+    def moveTo(self, loc):
+        self.loc.removeMonster(self)
+        self.loc = room
+        loc.addMonster(self)
     def die(self):
         self.room.removeMonster(self)
         updater.deregister(self)
 
+class TRex(Monster):
+    def __init__(self):
+        Monster.__init__(self,"Angry T-Rex",500, .75, 20)
+
+class Pterodactyl(Monster):
+    def __init__(self):
+        Monster.__init__(self,"Pterodactyl",100, .25, 10)
