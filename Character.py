75f908794243fb97af902b505e26c06204fb89b5
Amy Rose Lazarte <lazartea@reed.edu>
2016-12-06 19:36:09 -0800

    Update Character.py

--- Character.py
+++ Character.py
@@ -3,27 +3,20 @@ import updater
 from item import Item 
 
 class Merchant:
-    def __init__(self, name, inven, room):
-        self.name = name
-        self.room = room
+    def __init__(self, inven):
+        self.name = "Stegosaurus"
+        self.room = None
         self.inven = inven
-        room.addMer(self)
-        updater.register(self)
+        
+        
 
-
-    def update(self):
-        return
-    
     def getItemByName(self, name):
         for i in self.inven:
             if i.name.lower() == name.lower():
                 return i
         return False
             
-    def moveTo(self, room):
-        self.room.removeMer(self)
-        self.room = room
-        room.addMer(self)
-    def die(self):
-        self.room.removeMer(self)
-        updater.deregister(self)
+    def putInRoom(self, room): #merchants do not move once they are placed
+        self.loc = room
+        self.loc.addMer(self)
+
